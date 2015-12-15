34493c260804e162c161f5ecfab7f6163f4d7153
metlem <meltemdzdr@gmail.com>
2015-12-15 20:57:33 +0200

    Veritabanındaki postları görebilmek için şablonları değiştirdim.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-def post_list(request):     
-    return render(request, 'blog/post_list.html', {})
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
