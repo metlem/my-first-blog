d7aaf3d89754515c4ab923e5ed9377941cbeb8d9
istkulupblog <meltemdzdr@gmail.com>
2016-08-04 02:20:05 +0300

    Taslak ozelligi ekllendi

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.yazar = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
@@ -30,10 +30,26 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.yazar = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(yayinlanma_tarihi__isnull=True).order_by('yaratilis_tarihi')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
 
